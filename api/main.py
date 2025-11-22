ce18d7a112c01e0d676a8b4abb7ccba51aef402c
mungai-dev <allankinuthia602@gmail.com>
2025-11-22 19:51:18 +0300

    route for generate qr

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from auth_token import getaccess_token
 from stk import stk_push
+from qrgenerate import GenerateQR
 
 app = FastAPI()
 
@@ -10,6 +11,9 @@ def home():
 
 @app.get("/get_token")
 def get_token():
+    #returns 2 objects:access_token and expires_in
+    #access_token - enables access to the APIs
+    #expires_in - token expiry time in seconds.
     access_token = getaccess_token()
     return access_token
 
@@ -18,5 +22,8 @@ def stk_code():
     stk = stk_push()
     return stk
 
-
+@app.post("/generate_token")
+def generate_qr():
+    generate = GenerateQR()
+    return generate
 
