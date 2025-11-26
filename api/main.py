bc9cd700df5322eb965ac97c80f4fb62a11b4d84
kinus-bit <mungaiallan540@gmail.com>
2025-11-26 19:46:53 +0300

    cors error

--- api/main.py
+++ api/main.py
@@ -7,8 +7,22 @@ import requests
 import base64
 from datetime import datetime
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+origins = [
+	"http://localhost:5500"
+]
+
+app.add_middleware(
+	CORSMiddleware,
+ allow_origins = origins,
+ allow_credentials = True,
+ allow_methods = ["*"],
+ allow_headers = ["*"]
+ 
+)
 class PayRequest(BaseModel):
     phone: str
     price: str
@@ -97,40 +111,40 @@ def stk_push(phone: str, price: str):
 # ========== QR GENERATE MODULE ==========
 #api for generating a dynamic M-PESA QR Code.
 #post url
-STK_URL = "https://sandbox.safaricom.co.ke/mpesa/qrcode/v1/generate"
+# STK_URL = "https://sandbox.safaricom.co.ke/mpesa/qrcode/v1/generate"
 
-def GenerateQR():
-    try:
+# def GenerateQR():
+#     try:
         #calling access token - we moved it inside function as when we call it at import
         # time,it can fail in serverless environment
-        token_response = getaccess_token()
+        # token_response = getaccess_token()
 
         #error handling before we access the access_token
-        if 'error' in token_response:
-            return {"error": f"Failed to get access token: {token_response['error']}"}
-        access_token = token_response['access_token']
+        # if 'error' in token_response:
+        #     return {"error": f"Failed to get access token: {token_response['error']}"}
+        # access_token = token_response['access_token']
         
         #headers with content type and authhorization
-        header = {
-            "content-Type":"application/json",
-            "Authorization":f'Bearer {access_token}'
-        }
+        # header = {
+        #     "content-Type":"application/json",
+        #     "Authorization":f'Bearer {access_token}'
+        # }
         
         #this is the request body
-        pay_info ={
-            "MerchantName":"TEST SUPERMARKET",
-            "RefNo":"Invoice Test",
-            "Amount":1,
-            "TrxCode":"SM",
-            "CPI":"0705102180",
-            "Size":"300"
-            }
+        # pay_info ={
+        #     "MerchantName":"TEST SUPERMARKET",
+        #     "RefNo":"Invoice Test",
+        #     "Amount":1,
+        #     "TrxCode":"SM",
+        #     "CPI":"0705102180",
+        #     "Size":"300"
+        #     }
         #we pass url,headers and json as params for post
-        response = requests.post(url=STK_URL ,headers=header,json=pay_info)
-        return response.json()
+    #     response = requests.post(url=STK_URL ,headers=header,json=pay_info)
+    #     return response.json()
     
-    except Exception as error:
-        return {'error':str(error)}
+    # except Exception as error:
+    #     return {'error':str(error)}
         
 
 # ========== FASTAPI ROUTES ==========
@@ -154,11 +168,11 @@ def stk_code(request: PayRequest):
     except Exception as e:
         return {"error": str(e)}
 
-@app.post("/generate_QR")
-def generate_qr():
-    try:
-        generate = GenerateQR()
-        return generate
-    except Exception as e:
-        return {"error": str(e)}
+# @app.post("/generate_QR")
+# def generate_qr():
+#     try:
+#         generate = GenerateQR()
+#         return generate
+#     except Exception as e:
+#         return {"error": str(e)}
 
